3f366b43d5275e41d21d902105dfefa304acfcd3
manikaS16 <manika.sharma@ltu.se>
2025-07-14 12:51:03 +0200

    Update etruck.ino

--- etruck.ino
+++ etruck.ino
@@ -1,20 +1,38 @@
 #include <Servo.h>
 
-Servo esc;
+Servo esc;      // ESC for motor
+Servo steer;    // Servo for steering
+
 const int escPin = A0;
+const int steerPin = A3;
 
 void setup() {
   Serial.begin(9600);
   esc.attach(escPin);
+  steer.attach(steerPin);
   delay(2000);    // Wait 2 seconds for ESC to initialize
   stopMotor();
+  centerSteer();
 }
 
 void loop() {
   if (Serial.available()) {
-    int angle = Serial.parseInt();
-    angle = constrain(angle, 0, 180);
-    moveMotor(angle);
+    String input = Serial.readStringUntil('\n');  // Read until newline
+
+    int commaIndex = input.indexOf(',');
+    if (commaIndex > 0) {
+      String motorStr = input.substring(0, commaIndex);
+      String steerStr = input.substring(commaIndex + 1);
+
+      int motorAngle = motorStr.toInt();
+      int steerAngle = steerStr.toInt();
+
+      motorAngle = constrain(motorAngle, 45, 135);   // safe range
+      steerAngle = constrain(steerAngle, 45, 135);
+
+      moveMotor(motorAngle);
+      moveSteer(steerAngle);
+    }
   }
 }
 
@@ -23,5 +41,13 @@ void moveMotor(int angle) {
 }
 
 void stopMotor() {
-  esc.write(90);
+  esc.write(90);   // Neutral ESC position to stop motor
+}
+
+void moveSteer(int angle) {
+  steer.write(angle);
+}
+
+void centerSteer() {
+  steer.write(90);  // Center steering servo
 }
